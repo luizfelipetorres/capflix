28ed5d9221f32859ecf12f885e1e1f34be860626
luizfelipetorres <fellipe116@gmail.com>
2022-10-11 09:30:49 -0300

    Add click listeners and view binding

--- app/src/main/java/com/lftf/capflix/MainActivity.java
+++ app/src/main/java/com/lftf/capflix/MainActivity.java
@@ -1,14 +1,55 @@
 package com.lftf.capflix;
 
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
+import com.lftf.capflix.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this));
+        setContentView(binding.getRoot());
+
+        binding.iconShowPassword.setOnClickListener(this);
+        binding.forgotPassword.setOnClickListener(this);
+        binding.buttonLogin.setOnClickListener(this);
+        binding.textViewSignUp.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.icon_show_password:
+                if (binding.editTextPassword.getInputType() != InputType.TYPE_CLASS_TEXT)
+                    binding.editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT);
+                else
+                    binding.editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                break;
+            case R.id.button_login:
+                Snackbar.make(this ,v, "Fazendo login...", Snackbar.LENGTH_LONG).show();
+                break;
+            case R.id.forgot_password:
+                Snackbar.make(v, "Esqueci a senha...", Snackbar.LENGTH_LONG).show();
+                break;
+            case R.id.text_view_sign_up:
+                Snackbar.make(v, "Criar usuário...", Snackbar.LENGTH_LONG).show();
+
+        }
     }
 }
